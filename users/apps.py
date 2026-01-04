91f74f8207106d7c0b90d12f664174d3311fd09a
ujjalmukthakim <ujjalahammed2143@gmail.com>
2026-01-05 04:31:26 +0600

    wow6

--- users/apps.py
+++ users/apps.py
@@ -1,27 +1,37 @@
 from django.apps import AppConfig
 import os
 from django.contrib.auth import get_user_model
+from django.db.utils import OperationalError
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
 
     def ready(self):
-        from django.db.utils import OperationalError
         User = get_user_model()
+
         try:
             username = os.environ.get("CEO_USERNAME")
             email = os.environ.get("CEO_EMAIL")
             password = os.environ.get("CEO_PASSWORD")
 
-            if username and email and password:
-                if not User.objects.filter(username=username).exists():
-                    User.objects.create_superuser(
-                        username=username,
-                        email=email,
-                        password=password,
-                        role="CEO"  # make sure your User model has a 'role' field
-                    )
+            if not all([username, email, password]):
+                return
+
+            user, created = User.objects.get_or_create(
+                username=username,
+                defaults={"email": email},
+            )
+
+            if created:
+                user.set_password(password)
+
+            # 🔥 FORCE CEO POWERS EVERY TIME
+            user.role = "CEO"
+            user.status = "Approved"
+            user.is_staff = True
+            user.is_superuser = True
+            user.save()
+
         except OperationalError:
-            # Happens if DB isn’t ready yet (migrations not run)
             pass
