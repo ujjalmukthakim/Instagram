7ecc814b69552e30f91952721d3f004697d198c8
ujjalmukthakim <ujjalahammed2143@gmail.com>
2026-01-04 12:42:17 +0600

    ppppppp

--- backend/settings.py
+++ backend/settings.py
@@ -28,6 +28,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
+DEBUG = os.environ.get("DEBUG") == "True"
+
 
 ALLOWED_HOSTS = ['*']
 
