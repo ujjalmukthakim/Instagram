a002f307c57d638d103ea689f09f7a03530ff120
ujjalmukthakim <ujjalahammed2143@gmail.com>
2026-01-05 01:43:57 +0600

    done

--- backend/settings.py
+++ backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY","django-insecure-2cr+q#55uv-tzt9cj9*kcj07@0%2u703deu)c#p6%ch57$-fdr")
 
 # SECRET_KEY = 'django-insecure-2cr+q#55uv-tzt9cj9*kcj07@0%2u703deu)c#p6%ch57$-fdr'
 
--- users/apps.py
+++ users/apps.py
@@ -1,6 +1,27 @@
 from django.apps import AppConfig
-
+import os
+from django.contrib.auth import get_user_model
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+
+    def ready(self):
+        from django.db.utils import OperationalError
+        User = get_user_model()
+        try:
+            username = os.environ.get("CEO_USERNAME")
+            email = os.environ.get("CEO_EMAIL")
+            password = os.environ.get("CEO_PASSWORD")
+
+            if username and email and password:
+                if not User.objects.filter(username=username).exists():
+                    User.objects.create_superuser(
+                        username=username,
+                        email=email,
+                        password=password,
+                        role="CEO"  # make sure your User model has a 'role' field
+                    )
+        except OperationalError:
+            # Happens if DB isn’t ready yet (migrations not run)
+            pass
