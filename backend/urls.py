3a46d1521ed1dc05c324cabea926bb3cd8213df9
ujjalmukthakim <ujjalahammed2143@gmail.com>
2026-01-05 04:23:23 +0600

    wow65

--- backend/urls.py
+++ backend/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('sadmin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/groups/', include('groups.urls')),
     path('api/dashboard/',include('api.urls')),
